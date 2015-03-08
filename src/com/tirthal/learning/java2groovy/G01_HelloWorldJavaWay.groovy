e8311b50dd06e70091abebc648e850055619490d
Tirthal Patel <patel.tirthal@gmail.com>
2015-03-08 14:43:30 +0530

    Did trivial change

--- src/com/tirthal/learning/java2groovy/G01_HelloWorldJavaWay.groovy
+++ src/com/tirthal/learning/java2groovy/G01_HelloWorldJavaWay.groovy
@@ -2,18 +2,18 @@ package com.tirthal.learning.java2groovy;
 
 /*
  * DEFAULT IMPORTS:
- * 
- * You don’t have to import some common classes/packages when you write Groovy code. 
- * For example, Calendar readily refers to java.util.Calendar. 
- * 
- * Groovy automatically imports the following Java packages: 
- * 		java.lang, 
- * 		java.util, 
+ *
+ * You don’t have to import some common classes/packages when you write Groovy code.
+ * For example, Calendar readily refers to java.util.Calendar.
+ *
+ * Groovy automatically imports the following Java packages:
+ * 		java.lang,
+ * 		java.util,
  * 		java.io,
- * 		java.net. 
- * 
- * It also imports the classes java.math.BigDecimal and java.math.BigInteger. 
- * 
+ * 		java.net.
+ *
+ * It also imports the classes java.math.BigDecimal and java.math.BigInteger.
+ *
  * In addition, the Groovy packages groovy.lang and groovy.util are imported.
  */
 
@@ -23,7 +23,7 @@ public class G01_HelloWorldJavaWay {
 	public static void main(String[] args) {
 
 		for(int i=0; i<3; i++) {
-			System.out.println("Hello World");
+			System.out.println("Hello World" + i);
 		}
 	}
 }
--- src/com/tirthal/learning/java2groovy/G02_HelloWorldGroovyWay.groovy
+++ src/com/tirthal/learning/java2groovy/G02_HelloWorldGroovyWay.groovy
@@ -2,7 +2,7 @@ package com.tirthal.learning.java2groovy
 
 // Remove the class and method definition, as not required in Groovy
 for(int i = 0; i < 3; i++) {
-	System.out.println("Hello World" )
+	System.out.println("Hello World $i")
 }
 
 // Other ways of the for loop - in below loop "it" is an implicit variable
@@ -17,4 +17,4 @@ for(i in 0..2) { println "Hello World $i" }
 3.times { println "Hello World $it" } // starts with 0
 
 // 4 - skip values while looping, using the step( ) method
-0.step(10, 2) { println "Hello World $it " }	
+0.step(10, 2) { println "Hello World $it " }
